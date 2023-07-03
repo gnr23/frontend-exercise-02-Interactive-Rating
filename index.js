fa14277941ac88118ba0427e48429d64b1273600
gnr23 <agrios.constantinos@gmail.com>
2023-07-03 14:10:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,25 +19,4 @@ submitButton.addEventListener("click", () => {
 rateAgain.addEventListener("click", () => {
   mainContainer.style.display = "block"
   thanksContainer.classList.add("hidden")
-}const mainContainer = document.querySelector(".container")
-const thanksContainer = document.querySelector(".thank-you")
-const submitButton = document.getElementById("submit-rating")
-const rateAgain = document.getElementById("rate-again")
-const ratings = document.querySelectorAll(".btn")
-const actualRating = document.getElementById("rating")
-
-submitButton.addEventListener("click", () => {
-  mainContainer.style.display = "none"
-  thanksContainer.classList.remove("hidden")
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      actualRating.innerHTML = rating.innerHTML
-    })
-  })
 })
-
-rateAgain.addEventListener("click", () => {
-  mainContainer.style.display = "block"
-  thanksContainer.classList.add("hidden")
-})￼Enter
